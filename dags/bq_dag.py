6f04258a85557d07c7d995b5e0b6de70d169f4b2
Gabriele Di Bernardo <gdibernardo@bol.com>
2019-02-25 17:18:35 +0100

    Add slackclient on requirements.

--- dags/bq_dag.py
+++ dags/bq_dag.py
@@ -9,7 +9,7 @@ dag = DAG(
     schedule_interval='@daily',
     default_args={
         'owner': 'GoDataDriven',
-        'start_date': airflow.utils.dates.days_ago(2)
+        'start_date': airflow.utils.dates.days_ago(14)
     }
 )
 
